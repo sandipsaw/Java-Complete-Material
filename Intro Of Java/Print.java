00ad5eebb33c230c54fc0ead152623aea17c1b31
sandip saw <sandipsaw449@gmail.com>
2025-10-08 22:57:33 +0530

    Variable and DataType & various way to Print the Output

---
+++ Intro Of Java/Print.java
@@ -0,0 +1,18 @@
+public class Print {
+
+    public static void main(String args[]){
+        // There are three method to print Output
+        System.out.println("println give next line after print output"); 
+        System.out.println("\n also give next line to print output, anywhere in double quote if i use \n then the output which is after the \n print in the next line");
+        System.out.print("Print do not give next line"); 
+
+        int age = 45;
+        int a= 5, b=10;
+        int sum = a + b;
+        System.out.println("\n sum = "+sum);
+        System.out.println("age = "+age);
+    }
+}
+// Variables
+// A variable is a container (storage area) used to hold data.  
+// Each variable should be given a unique name (identifier).  
